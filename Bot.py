b6fa7b819c2c6c56927e1c6997a6a5f7e880cd39
Joel-Scott-APSU <joel.scott901@gmail.com>
2024-03-19 23:10:32 -0500

    Fixed Jokes.py to read from all text files, corrected db disconnect, Fixed db issues when adding jokes

--- Bot.py
+++ Bot.py
@@ -1,7 +1,10 @@
-from Jokes import read_text_file, read_jokes
+from Jokes import read_text_file, read_random_jokes, read_dark_jokes
 import discord
 from discord.ext import commands 
 from DatabaseConfig import BOT_TOKEN
+from DatabaseConfig import connect_to_database
+
+db_cursor, db_connection = connect_to_database()
 
 intents = discord.Intents.all()
 intents.messages = True
@@ -9,16 +12,25 @@ intents.guilds = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.command(name = "Joke", help = "Tells a random joke")
-async def tell_joke(ctx):
-    joke = read_jokes('alljokes')
+async def tell_random_jokes(ctx):
+    joke = read_random_jokes('alljokes')
+    await ctx.send(joke)
+
+@bot.command(name = "DarkJoke", help = "Tells a dark humored joke")
+async def tell_dark_jokes(ctx):
+    joke = read_dark_jokes('darkhumor')
     await ctx.send(joke)
 
 @bot.event
 async def on_ready():
-    filenames = ["Jokes/300RandomJokes.txt", "Jokes/DarkHumorJokes.txt"]
+    filenames = ["300RandomJokes.txt", "DarkHumorJokes.txt"]
     
     for filename in filenames:
         read_text_file(filename)
 
-
+@bot.event
+async def on_disconnect():
+    db_cursor.close()
+    db_connection.close()
+    
 bot.run(BOT_TOKEN)
--- Jokes.py
+++ Jokes.py
@@ -1,46 +1,62 @@
 import re
 from DatabaseConfig import connect_to_database
-import os
 
 db_cursor, db_connection = connect_to_database()
 
 def read_text_file(filename):
     try:
+        print(filename)
         with open(filename, 'r', encoding='utf-8') as file:
             text = file.read()  # Read the entire file into a string
-            
             # Use regular expression to match the patterns for jokes and punchlines
-            filename = os.path.basename(filename)
+            
             if filename == '300RandomJokes.txt':
-                joke_punchline_pairs = re.findall(r'\d+\. (.+?[?.!]) (.+)', text)
+                joke_punchline_pairs = re.findall(r'\d+\.\s*(.+?[?.!])\s*(.+)', text)
                 table_name = 'alljokes'
+                            # Iterate over each joke-punchline pair
+                for joke, punchline in joke_punchline_pairs:
+                    # Check if the joke-punchline pair already exists in the database
+                    db_cursor.execute(f"SELECT * FROM {table_name} WHERE joke = %s AND punchline = %s", (joke, punchline))
+                    if not db_cursor.fetchall():
+                        # If the pair doesn't exist, insert it into the database
+                        db_cursor.execute(f"INSERT INTO {table_name} (joke, punchline) VALUES (%s, %s)", (joke, punchline))
+                        db_connection.commit()
+
             elif filename == 'DarkHumorJokes.txt':
                 joke_punchline_pairs = re.findall(r'\d+\.\s*(.+)', text)
                 table_name = 'darkhumor'
+                
+                for joke in joke_punchline_pairs:
+                    db_cursor.execute(f"SELECT * FROM {table_name} WHERE JOKE = %s", (joke,))
+
+                    if not db_cursor.fetchall():
+                        db_cursor.execute(f'INSERT INTO {table_name} (joke) VALUES (%s)', (joke,))
+                        db_connection.commit()
+
             else:
                 print("Unknown filename. Data will not be inserted.")
                 return
-            
-            # Iterate over each joke-punchline pair
-            for joke, punchline in joke_punchline_pairs:
-                # Check if the joke-punchline pair already exists in the database
-                db_cursor.execute(f"SELECT * FROM {table_name} WHERE joke = %s AND punchline = %s", (joke, punchline))
-                if not db_cursor.fetchall():
-                    # If the pair doesn't exist, insert it into the database
-                    db_cursor.execute(f"INSERT INTO {table_name} (joke, punchline) VALUES (%s, %s)", (joke, punchline))
-                    db_connection.commit()
     except FileNotFoundError:
         print("File not found.")
     except Exception as e:
         print(f"There was an error adding files to the database: {e}")
 
-def read_jokes(table):
+def read_random_jokes(table):
     db_cursor = db_connection.cursor()
 
-    db_cursor.execute(f"SELECT * FROM {table} ORDER BY RAND() LIMIT 1")
-    joke = db_cursor.fetchone()
-    
-    db_cursor.close()
-    db_connection.close()
+    db_cursor.execute(f"SELECT joke, punchline FROM {table} ORDER BY RAND() LIMIT 1")
+    joke_tuple = db_cursor.fetchone()
+
+    joke = ' '.join([value.strip("'") for value in joke_tuple])
+
+    return joke
+
+def read_dark_jokes(table):
+    db_cursor = db_connection.cursor()
+
+    db_cursor.execute(f'SELECT joke FROM {table} ORDER BY RAND() LIMIT 1')
+    new_joke = db_cursor.fetchone()
+
+    joke = ' '.join([value.strip("'") for value in new_joke])
 
     return joke
